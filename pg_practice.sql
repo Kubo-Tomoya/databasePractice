7c8833391a2eb057c2c8e90351a12c6be7d68a16
久保友哉 <t.kubo.cingroup2012@gmail.com>
2025-03-31 03:19:09 +0900

    pg_practice.sqlを編集

--- pg_practice.sql
+++ pg_practice.sql
@@ -1,5 +1,21 @@
 -- 問題1. postgresqlでpracticeデータベースを生成するSQL文を記述してください。
 
+CREATE DATABASE practice;
+
 -- 問題2. postgresqlでpracticeデータベースのusersテーブルを生成するSQL文を記述してください。
 
+CREATE TABLE "users" (
+    "id" SERIAL PRIMARY KEY,
+    "name" CHAR(255) NOT NULL DEFAULT '' COMMENT '氏名',
+    "age" INT NOT NULL DEFAULT 0 COMMENT '年齢',
+    "gender" VARCHAR(10) NOT NULL DEFAULT 'Other' COMMENT '性別'
+);
+
 -- 問題3. postgresqlでpracticeデータベースのjobsテーブルを生成するSQL文を記述してください。
+
+CREATE TABLE "jobs" (
+    "id" SERIAL PRIMARY KEY,
+    "user_id" INT NOT NULL,
+    "name" CHAR(255) NOT NULL DEFAULT '会社員',
+    CONSTRAINT "fk_user_id" FOREIGN KEY ("user_id") REFERENCES "users"("id")
+);
