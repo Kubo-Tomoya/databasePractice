3c2a60a25bc2f9a4034dc9c8412d3e44ab2be4df
久保友哉 <t.kubo.cingroup2012@gmail.com>
2025-04-01 16:11:58 +0900

    修正依頼を実装

--- pg_practice.sql
+++ pg_practice.sql
@@ -4,12 +4,19 @@ CREATE DATABASE practice;
 
 -- 問題2. postgresqlでpracticeデータベースのusersテーブルを生成するSQL文を記述してください。
 
-CREATE TABLE "users" (
-    "id" SERIAL PRIMARY KEY,
-    "name" CHAR(255) NOT NULL DEFAULT '' COMMENT '氏名',
-    "age" INT NOT NULL DEFAULT 0 COMMENT '年齢',
-    `gender` enum('Man','Woman','Other') NOT NULL DEFAULT 'Other' Comment '性別',
+CREATE TYPE gender_type AS ENUM ('Man', 'Woman', 'Other');
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL DEFAULT '',
+    age INT NOT NULL DEFAULT 0,
+    gender gender_type NOT NULL DEFAULT 'Other'
 );
+
+COMMENT ON COLUMN users.name IS '氏名';
+COMMENT ON COLUMN users.age IS '年齢';
+COMMENT ON COLUMN users.gender IS '性別';
+
 
 -- 問題3. postgresqlでpracticeデータベースのjobsテーブルを生成するSQL文を記述してください。
 
